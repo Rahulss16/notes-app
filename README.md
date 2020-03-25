69b1f11bcd237fea82224c230c90c9bc26ed1943
Rahul Singh Shekhawat <rahulsinghshekhawat16@gmail.com>
2020-03-25 12:39:55 +0530

    Install packages and use in code to understand how is it work.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # notes-app
+run ## npm install
+npm install will install all the dependencies regarding to project. 
 Node.js :- Importing Node.js Core Modules, Importing Your Own Files &amp; Importing NPM modules.
+Learn import npm packages and how to use in code.
--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+const validator = require('validator')
+const chalk = require('chalk')
 const fs = require('fs');
 const getNotes = require('./notes.js');
 
+// fs libarary is file system from nodeJS
+// getNotes show how to import own module in node. it export title & body for notes.
 fs.writeFileSync('notes.txt', getNotes().title)
 fs.appendFileSync('notes.txt', getNotes().body)
 
+
+//example of validator package
+console.log(validator.isEmail('rahul@24x7host.com'))
+
+// example of chalk package
+console.log(chalk.green.inverse.bold(getNotes().title));
